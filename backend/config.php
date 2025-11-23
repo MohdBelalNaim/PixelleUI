72dbfd86fd138509d5c330819bdfce51acc8a5d7
Mohd Belal Naim <44068063+MohdBelalNaim@users.noreply.github.com>
2025-11-23 21:23:52 +0530

    Added all supporting files for backend

---
+++ backend/config.php
@@ -0,0 +1,21 @@
+<?php
+//database connection
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$conn = mysqli_connect("localhost","root","","pixelle-new");
+if(!$conn){
+    print_r("Cannot connect to database");
+    die();
+}
+
+$method = isset($method) ? $method : 'POST';
+//reading user input
+$jsonData = file_get_contents('php://input');
+if ($_SERVER['REQUEST_METHOD'] === $method) {
+   $data = json_decode($jsonData, true);
+}else{
+    echo "Invalid request method";
+    die();
+}
+?>
